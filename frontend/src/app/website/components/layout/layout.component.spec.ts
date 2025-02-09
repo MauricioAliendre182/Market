ea60f2da623a9cfe3f470db2a3fc67c508dccf45
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2025-02-09 11:34:28 -0400

    Fix layout.component test to import and declare the correct modules and components

--- frontend/src/app/website/components/layout/layout.component.spec.ts
+++ frontend/src/app/website/components/layout/layout.component.spec.ts
@@ -1,15 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NavComponent } from '../nav/nav.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [LayoutComponent]
-    });
+      declarations: [LayoutComponent, NavComponent], // ✅ Add NavComponent
+      imports: [RouterTestingModule, HttpClientTestingModule], // ✅ Add RouterTestingModule and HttpClientTestingModule
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
